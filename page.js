eb2b7d0eba3e004846b824b65b8635a01771dab9
doniswalter897-source <doniswalter897@gmail.com>
2025-11-20 01:46:39 -0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <div style={{padding:40}}>
+      <h1>Donis Car Detailing</h1>
+      <p>Your full English site will be added here next.</p>
+    </div>
+  );
+}
